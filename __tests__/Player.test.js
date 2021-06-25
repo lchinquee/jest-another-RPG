22f2a4d482c974b0ab4506e0aed58231d715c0c4
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-06-25 18:10:47 -0400

    add player health tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -39,3 +39,36 @@ test('gets inventory from player or returns false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+// Player health test
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+// Player health calculation test
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+    
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
+
+// Player check alive status test
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
